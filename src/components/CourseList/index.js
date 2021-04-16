ec382070e25b6277042fe32f8acbe7cea0c4b4b7
Thales Domingues <thdomingues.dev@gmail.com>
2021-04-15 21:32:46 -0400

    feat(courses): created action to insert data

--- src/components/CourseList/index.js
+++ src/components/CourseList/index.js
@@ -1,12 +1,26 @@
-import { useSelector } from 'react-redux';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 const CourseList = () => {
   const courses = useSelector(state => state.data);
+  const dispatch = useDispatch();
+
+  const [course, setCourse] = useState('');
+
+  function addCourse() {
+    dispatch({ type: 'ADD_COURSE', title: course });
+
+  }
 
   return (
-    <ul>
-      { courses.map(course => <li key={course}>{course}</li>)}
-    </ul>
+    <>
+      <ul>
+        {courses.map(course => <li key={course}>{course}</li>)}
+      </ul>
+
+      <input type="text" onChange={(e) => { setCourse(e.target.value) }} />
+      <button onClick={addCourse}>Add Course</button>
+    </>
   );
 }
 
--- src/store/index.js
+++ src/store/index.js
@@ -10,7 +10,7 @@ const INITIAL_STATE = {
 
 function courses(state = INITIAL_STATE, action) {
   switch (action.type) {
-    case 'ADD_CURSE':
+    case 'ADD_COURSE':
       return { ...state, data: [...state.data, action.title] };
 
     default:
